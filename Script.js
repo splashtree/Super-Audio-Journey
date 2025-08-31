f04b2e12aba0a593e76162b457cf655793ef366a
splashtree <spokes.tom@gmail.com>
2025-08-31 01:35:07 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Grab the audio element by its ID
+const player = document.getElementById("player");
+
+function playAudio() {
+  player.play();
+}
+
+function pauseAudio() {
+  player.pause();
+}
